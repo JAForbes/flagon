ea5a39a51f61fee782d1bd0eb7b025bb19013fe1
James Forbes <james.a.forbes@gmail.com>
2015-10-31 11:40:08 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var flag = (A) => ({
+  contains: (B) => flag(flag.contains(A,B)),
+  merge: (B) => flag(flag.merge(A,B)),
+  toggle: (B) => flag(flag.toggle(A,B)),
+  value: () => A,
+  toString: () => A.toString(2)
+})
+flag.contains = (A,B) => (A & B) == B
+flag.merge = (A,B) => A | B
+flag.toggle = (A,B) => A ^ B
+
+if( typeof module !== undefined ){
+  module.exports = flag
+}
